7c58650091735af5294ef8fb6daca844affc409c
naokikurihara <nk.tk2_rm@icloud.com>
2017-12-14 16:53:42 +0900

    backup

---
+++ Xperia/src/Xperia.java
@@ -0,0 +1,15 @@
+public class Xperia {
+ public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	 SmartPhone xperia=new SmartPhone();
+	 xperia.play();
+	 xperia.stop();
+	 xperia.next();
+	 xperia.back();
+	 xperia.call();
+	 xperia.mail();
+	 xperia.photo();
+	 xperia.internet();
+ }
+
+}
